8c471af15b7a2904caff7c7a14c7401acf625db3
linjpxc <815488987@qq.com>
2020-08-18 20:28:54 +0800

    新增扩展方法

---
+++ Zero.QuickMoney/Extensions/CurrencyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Zero.QuickMoney.Extensions
+{
+    public static class CurrencyExtensions
+    {
+        public static Money Money(this CurrencyInfo @this)
+             => new Money(@this, decimal.Zero);
+
+        public static Money Money(this CurrencyInfo @this, decimal amount)
+            => new Money(@this, amount);
+
+        public static CurrencyInfo GetCurrency(this RegionInfo @this)
+            => CurrencyInfo.FromRegion(@this);
+
+        public static CurrencyInfo GetCurrency(this CultureInfo @this)
+            => CurrencyInfo.FromCulture(@this);
+    }
+}
---
+++ Zero.QuickMoney/Extensions/MoneyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Zero.QuickMoney.Extensions
+{
+    public static class MoneyExtensions
+    {
+        public static int ToMinor(this Money @this)
+            => (int)(@this / @this.Currency.MinorUnit);
+
+        public static Money ConvertTo(this Money @this, CurrencyInfo quoteCurrency, decimal rate)
+        {
+            if (@this.Currency == quoteCurrency)
+            {
+                return @this;
+            }
+            return new ExchangeRate(@this.Currency, quoteCurrency, rate).Convert(@this);
+        }
+
+        public static Money ConvertTo(this Money @this, ExchangeRate rate)
+            => rate.Convert(@this);
+    }
+}
